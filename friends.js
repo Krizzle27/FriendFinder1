516d7e22c676ef096155af4f9b70786adc60ac1a
Krizzle27 <31429446+Krizzle27@users.noreply.github.com>
2018-07-24 18:05:58 -0600

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Harry Potter',
+		photo: 'https://techcrunch.com/wp-content/uploads/2017/11/harrypotter.jpg?w=730&crop=1',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'Ron Weasley',
+		photo: 'http://www.maskerix.de/wp-content/uploads/2017/01/harry-potter-ron-weasley-kostuem-selber-machen.jpg',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Hermione Granger',
+		photo: 'https://i.ytimg.com/vi/Fd6-u5NU2Yg/maxresdefault.jpg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Dumbledore',
+		photo: 'http://azcheta.com/azcheta-content/uploads/2015/11/3770585-9693277282-vlcsn-copy-1024x523-853x523.png',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
